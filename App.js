5d53710f239d60d74a9ff5d2d13011e7162be015
PrinceBobo <usmanfrasu@gmail.com>
2020-04-19 15:51:52 +0500

    Pushing 1st updated userData.js

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import {createStackNavigator} from '@react-navigation/stack';
 import {ImageBackground, Button, StyleSheet, Text, View, Alert, TextInput, Image} from 'react-native';
 
 import login from './pages/login';
+import userdata from './pages/userdata';
 import sessionsMenu from './pages/sessionsMenu';
 //import { divide } from 'react-native-reanimated';
 
@@ -19,6 +20,7 @@ export default class App extends React.Component {
      <NavigationContainer>
         <stack.Navigator initialRouteName="Login">
             <stack.Screen name="Login" component={login} options={{ headerShown: false}}/>
+            <stack.Screen name="UserData" component={userdata} options={{ headerShown: false}}/>
             <stack.Screen name="Sessions Menu" component={sessionsMenu} options={{ headerShown: false}}/>
         </stack.Navigator>
      </NavigationContainer>
